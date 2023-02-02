950bdfb7e6f75ddc9dd58d7e74dcfafff97c5795
franromeroe <franromeroeze@gmail.com>
2023-02-02 13:17:26 -0300

    franco e romero

--- main.js
+++ main.js
@@ -79,3 +79,19 @@ document.querySelectorAll(".gallery__img img").forEach(el=>{
 		this.parentNode.classList.add("active");
 	})
 });
+
+
+//video
+
+
+const clip = document.querySelectorAll('.clip');
+
+for(let i = 0; i < clip.length; i++){
+    clip[i].addEventListener('mouseenter', function(e){
+        clip[i].play()
+    })
+
+    clip[i].addEventListener('mouseout', function(e){
+        clip[i].pause()
+    })
+}
