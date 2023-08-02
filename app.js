f7fc79686c6a1a8ade5fc7d9d718ff6ad7a8dbd7
franromeroe <franromeroeze@gmail.com>
2023-08-02 14:57:19 -0300

    portafolio

--- app.js
+++ app.js
@@ -379,11 +379,21 @@ phone.addEventListener('click', function(e){
 
 const carrusel = document.querySelector('.carrusel-items');
 
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
 let intervalo = null;
 let step = 1;
 const start = () =>{
   intervalo = setInterval(function(){
     carrusel.scrollLeft += step;
+    // if(carrusel.scrollLeft >= maxScrollLeft){
+    //   // console.log('hola');
+    // }
+    // // if(carrusel.scrollLeft === maxScrollLeft){
+    // //   step = step * -1;
+    // // }
+    // // else if(carrusel.scrollLeft <= 0){
+    // //   step = step * -1;
+    // // }
   },10)
 }
 
